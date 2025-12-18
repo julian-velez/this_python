ac61400d6808fa0276bb41bd1fd4d44e9df869e0
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-12-18 15:12:50 -0500

    this_python
    this_python

---
+++ this/main.py
@@ -0,0 +1,3 @@
+#El Zen de Python es una colección de 19 principios filosóficos que guían el diseño y la escritura de código en el lenguaje Python, promoviendo la claridad, la legibilidad y la simplicidad; escrito por Tim Peters, se puede ver ejecutando import this en cualquier intérprete de Python, revelando un poema con estas ideas, como "Bonito es mejor que feo" y "La legibilidad cuenta". 
+
+import this
